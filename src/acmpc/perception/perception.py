7563fbc5b36973668e0be3a4333ad9b99aa43b27
XDynames <james.bockman@live.com.au>
2024-10-28 19:36:38 +1030

    Seperated segmentation and tracklimit extraction into processes

--- src/acmpc/perception/perception.py
+++ src/acmpc/perception/perception.py
@@ -1,6 +1,5 @@
 import io
 import multiprocessing as mp
-import signal
 from typing import Dict
 
 from PIL import Image
@@ -13,59 +12,56 @@ from acmpc.perception.segmentation import (
 )
 from acmpc.perception.shared_memory import SharedImage, SharedPoints
 from acmpc.perception.tracks import TrackLimitPerception
+from acmpc.worker.base import WorkerProcess
 import cv2
 from loguru import logger
 import numpy as np
 from turbojpeg import TJPF_BGRX, TurboJPEG
 
 TURBO_JPEG = TurboJPEG()
-Perception_Monitor = SystemMonitor(300)
+Track_Limits_Monitor = SystemMonitor(300)
 
 
 class Perceiver:
     def __init__(self, cfg: Dict):
-        self._perceiver = PerceptionProcess(cfg)
-        self.image_width = cfg["image_width"]
-        self.image_height = cfg["image_height"]
-        self._perceiver.start()
-        self._is_first_resize = True
+        self.__setup(cfg)
 
     @property
     def is_centreline_stale(self) -> bool:
-        return self._perceiver.is_centreline_stale
+        return self._tracklimit_extractor.is_centreline_stale
 
     @property
     def centreline(self) -> np.array:
-        return self._perceiver.centreline
+        return self._tracklimit_extractor.centreline
 
     @property
     def is_tracklimits_stale(self) -> bool:
-        return self._perceiver.is_tracklimits_stale
+        return self._tracklimit_extractor.is_tracklimits_stale
 
     @property
     def tracklimits(self) -> Dict:
-        return self._perceiver.tracklimits
+        return self._tracklimit_extractor.tracklimits
 
     @property
     def visualisation_tracks(self) -> Dict:
-        return self._perceiver.tracks
+        return self._tracklimit_extractor.tracks
 
     @property
     def input_image(self) -> np.array:
-        return self._perceiver.input_image
+        return self._segmentor.input_image
 
     @property
     def output_mask(self) -> np.array:
-        return self._perceiver.output_mask
+        return self._segmentor.output_mask
 
     @property
     def output_visualisation(self) -> np.array:
-        return self._perceiver.output_visualisation
+        return self._segmentor.output_visualisation
 
     def perceive(self, obs: ObservationDict):
         if not obs["is_image_stale"]:
             self._preprocess_observations(obs)
-            self._submit_image_to_perceiver(obs)
+            self._submit_image_to_segmentor(obs)
 
     def _preprocess_observations(self, obs: ObservationDict):
         obs["CameraFrontRGB"] = self._encode_decode_image(obs["CameraFrontRGB"])
@@ -103,40 +99,95 @@ class Perceiver:
     def _is_image_size_correct(self, image: np.array) -> bool:
         return image.shape[:2] == (self.image_height, self.image_width)
 
-    def _submit_image_to_perceiver(self, obs: ObservationDict):
-        self._perceiver.input_image = obs["CameraFrontRGB"]
+    def _submit_image_to_segmentor(self, obs: ObservationDict):
+        self._segmentor.input_image = obs["CameraFrontRGB"]
 
     def shutdown(self):
-        self._perceiver.is_running = False
+        self._segmentor.is_running = False
+        self._tracklimit_extractor.is_running = False
+
+    def __setup(self, cfg: Dict):
+        self._setup_workers(cfg)
+        self._unpack_config(cfg)
+        self._launch_workers()
+        self._is_first_resize = True
+
+    def _setup_workers(self, cfg: Dict):
+        self._segmentor = SegmentationProcess(cfg)
+        self._tracklimit_extractor = TrackExtractionProcess(cfg, self._segmentor)
+
+    def _unpack_config(self, cfg: Dict):
+        self.image_width = cfg["image_width"]
+        self.image_height = cfg["image_height"]
+
+    def _launch_workers(self):
+        self._segmentor.start()
+        self._tracklimit_extractor.start()
 
 
-class PerceptionProcess(mp.Process):
-    def __init__(self, cfg: Dict):
-        super().__init__()
-        self.__setup(cfg)
+class SegmentationProcess(WorkerProcess):
+    @property
+    def input_image(self) -> np.array:
+        return self._shared_input.image
+
+    @input_image.setter
+    def input_image(self, image: np.array):
+        self._shared_input.image = image
 
     @property
-    def is_running(self) -> bool:
-        """
-        Checks if the segmentation process is running
+    def output_mask(self) -> np.array:
+        return self._shared_mask.image
 
-        :return: True if the process is running, false if it is not
-        :rtype: bool
-        """
-        with self._is_running.get_lock():
-            is_running = self._is_running.value
-        return is_running
+    @property
+    def output_visualisation(self) -> np.array:
+        return self._shared_visualisation.image
 
-    @is_running.setter
-    def is_running(self, is_running: bool):
-        """
-        Sets if the segmentation process is running
+    @property
+    def is_mask_stale(self) -> bool:
+        return self._shared_mask.is_stale
 
-        :is_running: True if the process is running, false if it is not
-        :type is_running: bool
-        """
-        with self._is_running.get_lock():
-            self._is_running.value = is_running
+    def _runtime_setup(self):
+        self._segmenter._setup_segmentation_model()
+
+    def _work(self):
+        frame = self._get_new_frame()
+        self._segment_drivable_area(frame)
+        # Segmentation_Monitor.maybe_log_function_itterations_per_second()
+
+    def _get_new_frame(self) -> np.array:
+        return self._shared_input.fresh_image
+
+    def _segment_drivable_area(self, frame: np.array):
+        mask, vis = self._segmenter.segment_drivable_area(frame)
+        self._update_shared_images(mask, vis)
+
+    def _update_shared_images(self, mask: np.array, vis: np.array):
+        self._shared_mask.image = mask
+        self._shared_visualisation.image = vis
+
+    def _setup(self, cfg: Dict):
+        self._unpack_config(cfg)
+        self.__setup_segmenter(cfg)
+        self._allocate_shared_memory()
+
+    def _unpack_config(self, cfg: Dict):
+        self._width = cfg["image_width"]
+        self._height = cfg["image_height"]
+
+    def __setup_segmenter(self, cfg: Dict):
+        self._segmenter = TrackSegmenter(cfg)
+        # self._segmenter = TrackSegmenterTensorRT(cfg)
+
+    def _allocate_shared_memory(self):
+        self._shared_input = SharedImage(self._height, self._width, 3)
+        self._shared_mask = SharedImage(1, self._height, self._width)
+        self._shared_visualisation = SharedImage(1, self._height, self._width)
+
+
+class TrackExtractionProcess(WorkerProcess):
+    def __init__(self, cfg: Dict, segmentor: SegmentationProcess):
+        super().__init__(cfg)
+        self._shared_mask = segmentor._shared_mask
 
     @property
     def is_tracklimits_stale(self) -> bool:
@@ -161,26 +212,6 @@ class PerceptionProcess(mp.Process):
         with self._is_centreline_stale.get_lock():
             is_stale = self._is_centreline_stale.value
         return is_stale
-
-    @property
-    def input_image(self) -> np.array:
-        return self._shared_input.image
-
-    @input_image.setter
-    def input_image(self, image: np.array):
-        self._shared_input.image = image
-
-    @property
-    def output_mask(self) -> np.array:
-        return self._shared_mask.image
-
-    @property
-    def output_visualisation(self) -> np.array:
-        return self._shared_visualisation.image
-
-    @property
-    def is_mask_stale(self) -> bool:
-        return self._shared_mask.is_stale
 
     @property
     def tracklimits(self) -> Dict:
@@ -209,39 +240,15 @@ class PerceptionProcess(mp.Process):
             tracks["centre"] = self._shared_centre_track.points
         return tracks
 
-    def run(self):
-        """
-        Called on PerceptionProcess.start()
-        """
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        self._segmenter._setup_segmentation_model()
-        while self.is_running:
-            self._perception_work()
-            Perception_Monitor.maybe_log_function_itterations_per_second()
-            Segmentation_Monitor.maybe_log_function_itterations_per_second()
-
-    @track_runtime(Perception_Monitor)
-    def _perception_work(self):
-        frame = self._get_new_frame()
-        mask = self._segment_drivable_area(frame)
+    def _work(self):
+        mask = self._get_new_mask()
         self._extract_tracklimits(mask)
+        # Track_Limits_Monitor.maybe_log_function_itterations_per_second()
 
-    @track_runtime(Perception_Monitor)
-    def _get_new_frame(self) -> np.array:
-        return self._shared_input.fresh_image
+    def _get_new_mask(self) -> np.array:
+        return np.squeeze(self._shared_mask.fresh_image, axis=0)
 
-    @track_runtime(Perception_Monitor)
-    def _segment_drivable_area(self, frame: np.array) -> np.array:
-        mask, vis = self._segmenter.segment_drivable_area(frame)
-        self._update_shared_images(mask, vis)
-        return mask
-
-    @track_runtime(Perception_Monitor)
-    def _update_shared_images(self, mask: np.array, vis: np.array):
-        self._shared_mask.image = mask
-        self._shared_visualisation.image = vis
-
-    @track_runtime(Perception_Monitor)
+    @track_runtime(Track_Limits_Monitor)
     def _extract_tracklimits(self, mask: np.array):
         tracks = self._tracklimit_extractor(mask)
         self._update_centreline(tracks)
@@ -258,31 +265,25 @@ class PerceptionProcess(mp.Process):
             self._shared_centre_track.points = tracks["centre"]
             self._is_centreline_stale.value = False
 
-    def __setup(self, cfg: Dict):
-        self.__setup_config(cfg)
-        self.__setup_segmenter(cfg)
-        self.__setup_track_extractor(cfg)
-        self.__setup_shared_memory()
+    def _runtime_setup(self):
+        pass
 
-    def __setup_config(self, cfg: Dict):
+    def _setup(self, cfg: Dict):
+        self._unpack_config(cfg)
+        self._allocate_shared_memory()
+        self.__setup_track_extractor(cfg)
+
+    def _unpack_config(self, cfg: Dict):
         self._width = cfg["image_width"]
         self._height = cfg["image_height"]
         self._n_polyfit_points = cfg["n_polyfit_points"]
 
-    def __setup_segmenter(self, cfg: Dict):
-        self._segmenter = TrackSegmenter(cfg)
-        # self._segmenter = TrackSegmenterTensorRT(cfg)
-
-    def __setup_track_extractor(self, cfg: Dict):
-        self._tracklimit_extractor = TrackLimitPerception(cfg)
-
-    def __setup_shared_memory(self):
-        self._shared_input = SharedImage(self._height, self._width, 3)
-        self._shared_mask = SharedImage(1, self._height, self._width)
-        self._shared_visualisation = SharedImage(1, self._height, self._width)
+    def _allocate_shared_memory(self):
         self._shared_left_track = SharedPoints(self._n_polyfit_points, 2)
         self._shared_right_track = SharedPoints(self._n_polyfit_points, 2)
         self._shared_centre_track = SharedPoints(self._n_polyfit_points, 2)
-        self._is_running = mp.Value("i", True)
         self._is_tracklimits_stale = mp.Value("i", True)
         self._is_centreline_stale = mp.Value("i", True)
+
+    def __setup_track_extractor(self, cfg: Dict):
+        self._tracklimit_extractor = TrackLimitPerception(cfg)
